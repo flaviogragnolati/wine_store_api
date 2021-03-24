84a618748cb1073d1a9d719739f53913f1a8c90a
Flavio Gragnolati <flavio.gragnolati@gmail.com>
2021-03-24 20:47:12 -0300

    process env port for heroku

--- index.js
+++ index.js
@@ -17,12 +17,13 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+require('dotenv').config();
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-
+const PORT = process.env.PORT || 3000;
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3000, () => {
-    console.log('%s listening at 3000'); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
